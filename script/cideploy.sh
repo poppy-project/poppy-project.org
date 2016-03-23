b63680446777c5e798f51cb8429d6d9204c7d51a
Damien Caselli <damien.caselli@gmail.com>
2016-03-23 18:28:16 +0100

    Update ci

--- script/cideploy.sh
+++ script/cideploy.sh
@@ -1,5 +1,7 @@
+ls -lsa
 cd _site
 git init
+echo "Initialized git"
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 git remote add origin https://$GH_TOKEN@github.com/$GH_USERNAME/$GH_REPO.git
