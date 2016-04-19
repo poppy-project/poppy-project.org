0c318e28b5f09bf3ff0fdae27e6f9cac0b1b3cd6
Damien Caselli <damien.caselli@gmail.com>
2016-04-19 18:03:33 +0200

    Fix site.url and site.baseurl

--- _script/cibuild.sh
+++ _script/cibuild.sh
@@ -1,6 +1,8 @@
 # uncomment url in jekyll config
 sed 's/# url:/url:/g' '_config.yml' > _config.yml.new
 mv _config.yml.new _config.yml
+sed 's/# baseurl:/baseurl:/g' '_config.yml' > _config.yml.new
+mv _config.yml.new _config.yml
 
 jekyll build
 rm -rf .git
