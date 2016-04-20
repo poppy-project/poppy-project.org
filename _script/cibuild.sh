4032373031d694697a807ee95ceb73bf3f969902
Damien Caselli <damien.caselli@gmail.com>
2016-04-20 17:06:34 +0200

    Update deploy rules

--- _script/cibuild.sh
+++ _script/cibuild.sh
@@ -4,10 +4,8 @@ set -e # halt script on error
 # uncomment url in jekyll config
 sed 's/# url:/url:/g' '_config.yml' > _config.yml.new
 mv _config.yml.new _config.yml
-sed 's/# baseurl:/baseurl:/g' '_config.yml' > _config.yml.new
-mv _config.yml.new _config.yml
 
 bundle exec jekyll build --destination _site/poppy-project.org
-bundle exec htmlproofer ./_site --assume-extension --allow-hash-href
+bundle exec htmlproofer ./_site --assume-extension --allow-hash-href --check-html
 
 rm -rf .git
