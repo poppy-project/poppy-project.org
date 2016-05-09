f82057b37ffb1ca02aee6695ed95e22da0a3c00e
Damien Caselli <damien.caselli@gmail.com>
2016-05-09 16:08:11 +0200

    HTML proofer must ignore flowers.inria.fr url

--- _script/cibuild.sh
+++ _script/cibuild.sh
@@ -6,6 +6,6 @@ sed 's/# url:/url:/g' '_config.yml' > _config.yml.new
 mv _config.yml.new _config.yml
 
 bundle exec jekyll build --destination _site/poppy-project.org
-bundle exec htmlproofer ./_site --assume-extension --allow-hash-href --check-html
+bundle exec htmlproofer ./_site --assume-extension --allow-hash-href --check-html --url-ignore "/flowers.inria.fr/"
 
 rm -rf .git
