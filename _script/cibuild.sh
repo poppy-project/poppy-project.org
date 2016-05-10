adcff532b8db22adafd3a814b3095c86a09d7eef
Damien Caselli <damien.caselli@gmail.com>
2016-05-10 18:06:49 +0200

    Disable GA on dev environment

--- _script/cibuild.sh
+++ _script/cibuild.sh
@@ -4,6 +4,8 @@ set -e # halt script on error
 # uncomment url in jekyll config
 sed 's/# url:/url:/g' '_config.yml' > _config.yml.new
 mv _config.yml.new _config.yml
+sed 's/# analytics:/analytics:/g' '_config.yml' > _config.yml.new
+mv _config.yml.new _config.yml
 
 bundle exec jekyll build --destination _site/poppy-project.org
 bundle exec htmlproofer ./_site --assume-extension --allow-hash-href --check-html --url-ignore "/flowers.inria.fr/"
