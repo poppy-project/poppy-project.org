1312dc2b85dd68902d3e99e24807a5383dbc4fca
Damien Caselli <damien.caselli@gmail.com>
2016-06-13 18:06:55 +0200

    Fix htmlproofer ignored urls patterns
    artsciences.u-bordeaux.fr is returning random errors.

--- _script/test.sh
+++ _script/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bundle exec htmlproofer ./_site --assume-extension --allow-hash-href --check-html --url-ignore "/flowers.inria.fr/"
+bundle exec htmlproofer ./_site --assume-extension --allow-hash-href --check-html --url-ignore "/flowers.inria.fr/" --url-ignore "/artsciences.u-bordeaux.fr/"
